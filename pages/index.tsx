6cd8219b25c07d688bfc43f2e4e66fb5ee7ebcf4
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2022-07-31 16:24:51 +0900

    MUI導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Stack, Button } from '@mui/material'
+
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +18,12 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
 
         <p className={styles.description}>
           Get started by editing{' '}
